5c6381d9c2f3b3d7b5e89a24c9e7a83ab6c9b51d
Zach Banks <zbanks@mit.edu>
2014-09-29 21:59:20 -0400

    wave.c formatting

--- wave.c
+++ wave.c
@@ -6,33 +6,37 @@
 #include "globals.h"
 #include "error.h"
 
-int wave_new(node_t * node)
+int 
+wave_new(node_t * node)
 {
 	wave_state_t* state = malloc(sizeof(wave_state_t));
-    if(!state) return raise_error(ERR_MALLOC, node, "malloc failed");
+	if(!state) return raise_error(ERR_MALLOC, node, "malloc failed");
 
 	state->t = 0;
 	state->chunk = malloc(sizeof(double)*global_chunk_size);
-    if(!state->chunk) return raise_error(ERR_MALLOC, node, "malloc failed");
+	if(!state->chunk) return raise_error(ERR_MALLOC, node, "malloc failed");
 
-    node->state = state;
+	node->state = state;
 
 	return 0;
 }
 
-int wave_del(node_t * node)
+int 
+wave_del(node_t * node)
 {
 	free(((wave_state_t*)node->state)->chunk);
 	free(node->state);
-    return 0;
+	return 0;
 }
 
-static double f(double t)
+static double 
+f(double t)
 {
 	return sin(t*2*M_PI);
 }
 
-int wave_pull(node_t * node, void ** output)
+int 
+wave_pull(node_t * node, void ** output)
 {
 	double* freq=node->input_pull[0](&(node->input[0]));
 
@@ -48,6 +52,6 @@ int wave_pull(node_t * node, void ** output)
 
 	*output = ((void *) (state->chunk));
 
-    return 0;
+	return 0;
 }
 
