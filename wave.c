450d263604e18b2b378fbc6d3d94e104f9cf53d2
Eric Van Albert <eric@ericslaptop.(none)>
2014-10-04 06:59:56 -0400

    waveforms galore

--- wave.c
+++ wave.c
@@ -31,17 +31,32 @@ void wave_state_free(block_info_pt block_info, state_pt state)
 	free(state);
 }
 
-static double f(double t)
+static double sine(double t)
 {
 	return sin(t*2*M_PI);
+}
+
+static double saw(double t)
+{
+	return 2*fmod(t,1)-1;
+}
+
+static double square(double t)
+{
+	return 2*(fmod(t,1)<.5)-1;
 }
 
 error_t wave_pull(node_t * node, output_pt * output)
 {
 	double* freq;
+	int* wave;
+
 	error_t e;
 
 	e=pull(node,0,(output_pt*)(&freq));
+	if(e != SUCCESS) return e;
+
+	e=pull(node,1,(output_pt*)(&wave));
 	if(e != SUCCESS) return e;
 
 	wave_state_t* state = (wave_state_t*)(node->state);
@@ -50,7 +65,21 @@ error_t wave_pull(node_t * node, output_pt * output)
 
 	for(i=0;i<global_chunk_size;i++)
 	{
-		state->chunk[i] = f(state->t);
+		if(wave)
+		{
+			switch(*wave)
+			{
+				case 0:
+					state->chunk[i] = sine(state->t);
+					break;
+				case 1:
+					state->chunk[i] = saw(state->t);
+					break;
+				case 2:
+					state->chunk[i] = square(state->t);
+					break;
+			}
+		}
 		if(freq)
 		{
 			state->t += *freq/global_frame_rate;
