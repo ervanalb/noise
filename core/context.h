07cfc80a8d052058423deb9c11892d435aa7af38
Eric Van Albert <eric@van.al>
2015-10-30 00:40:14 -0400

    add blockclass tracking variables to context

--- core/context.h
+++ core/context.h
@@ -15,9 +15,15 @@ void nz_destroy_context(struct nz_context * context_p);
 // Interface for type system, block system, graph system
 
 struct nz_context {
+    // Typeclasses
     struct nz_typeclass const ** registered_typeclasses;
     size_t n_registered_typeclasses;
     size_t registered_typeclass_capacity;
+
+    // Blockclasses
+    struct nz_blockclass const ** registered_blockclasses;
+    size_t n_registered_blockclasses;
+    size_t registered_blockclass_capacity;
 };
 
 #endif
