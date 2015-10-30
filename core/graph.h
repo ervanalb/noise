bb747374c412985323fcfe1e5f55c3742eecc3ae
Eric Van Albert <eric@van.al>
2015-10-30 00:38:03 -0400

    fix up graph interface a little bit

--- core/graph.h
+++ core/graph.h
@@ -20,14 +20,11 @@ struct nz_graph;
 //     struct nz_node ** downstream_node_p_array;
 //     struct nz_node * next; // For linked list
 // }
-
-struct nz_graph {
-    const struct nz_context * context_p;
-    n_registered_typeclasses = 0;
-    registered_typeclass_capacity = 16;
-    registered_typeclasses = calloc(context_p->registered_typeclass_capacity, sizeof(struct nz_typeclass *));
-    struct nz_node * graph_node_array;
-}
+//
+// struct nz_graph {
+//     const struct nz_context * context_p;
+//     struct nz_graph * graph_node_head;
+/// }
 
 // --
 // Public interface
@@ -35,11 +32,11 @@ struct nz_graph {
 nz_rc nz_create_graph(const struct nz_context * context_p, struct nz_graph ** graph_pp);
 void nz_destroy_graph(struct nz_graph * graph_p);
 
-void nz_graph_add_block(struct nz_graph * graph_p, const char * id, const char * block, struct nz_block_info ** block_info_pp);
-void nz_graph_del_block(struct nz_graph * graph_p, const char * id);
-void nz_graph_connect(struct nz_graph * graph_p, const char * id_upstream, const char * port_upstream, const char * id_downstream, const char * port_downstream);
-void nz_graph_disconnect(struct nz_graph * graph_p, const char * id_upstream, const char * port_upstream, const char * id_downstream, const char * port_downstream);
-void nz_graph_inspect(const struct nz_graph * graph_p, const char * id, const char ** result);
-void nz_graph_send(const struct nz_graph * graph_p, const char * id, const char * string);
+nz_rc nz_graph_add_block(struct nz_graph * graph_p, const char * id, const char * block, struct nz_block_info ** block_info_pp);
+nz_rc nz_graph_del_block(struct nz_graph * graph_p, const char * id);
+nz_rc nz_graph_connect(struct nz_graph * graph_p, const char * id_upstream, const char * port_upstream, const char * id_downstream, const char * port_downstream);
+nz_rc nz_graph_disconnect(struct nz_graph * graph_p, const char * id_upstream, const char * port_upstream, const char * id_downstream, const char * port_downstream);
+nz_rc nz_graph_inspect(const struct nz_graph * graph_p, const char * id, const char ** result);
+nz_rc nz_graph_send(const struct nz_graph * graph_p, const char * id, const char * string);
 
 #endif
