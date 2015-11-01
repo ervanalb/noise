f0ab1bd61eacdb09d999c68fda0c2668277aa0b9
Eric Van Albert <eric@van.al>
2015-11-01 03:18:06 -0500

    add mising test apps

---
+++ app/test_block.c
@@ -0,0 +1,51 @@
+#include "noise.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+const size_t nz_chunk_size = 128;
+
+nz_rc run()
+{
+    nz_rc rc = NZ_SUCCESS;
+    struct nz_context * context;
+    const struct nz_blockclass * my_blockclass;
+    struct nz_block my_block;
+    struct nz_block_info my_block_info;
+    int a;
+    nz_obj * pull_result;
+    char * string;
+
+    if((rc = nz_context_create(&context)) == NZ_SUCCESS)
+    {
+        // Create a block
+        if((rc = nz_block_create(context, &my_blockclass, &my_block.block_state_p, &my_block_info, "constant(int,10)")) == NZ_SUCCESS)
+        {
+            pull_result = my_block_info.block_pull_fns[0](my_block, 0, (nz_obj*)&a);
+            if(pull_result == NULL) {
+                printf("pulled NULL\n");
+            } else {
+                printf("pulled %d\n", a);
+            }
+            my_blockclass->block_destroy(my_block.block_state_p, &my_block_info);
+        }
+    }
+    nz_context_destroy(context);
+    return rc;
+}
+
+int main()
+{
+    nz_rc rc = run();
+    if(rc != NZ_SUCCESS)
+    {
+        fprintf(stderr, "Noise error %d: %s\n", rc, nz_error_rc_str(rc));
+        fprintf(stderr, "File: %s line %d\n", nz_error_file, nz_error_line);
+        if(nz_error_string) {
+            fprintf(stderr, "%s\n", nz_error_string);
+            free(nz_error_string);
+        }
+        return 1;
+    }
+    return 0;
+}
+
---
+++ app/test_graph.c
@@ -0,0 +1,52 @@
+#include "noise.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+const size_t nz_chunk_size = 128;
+
+// TODO move this somewhere sane
+nz_rc debug_pull(struct nz_block *);
+
+nz_rc run()
+{
+    nz_rc rc = NZ_SUCCESS;
+    struct nz_context * context;
+    struct nz_graph * graph;
+    struct nz_block * block_handle;
+
+    if((rc = nz_context_create(&context)) == NZ_SUCCESS)
+    {
+        // Create a graph
+        if((rc = nz_graph_create(context, &graph)) == NZ_SUCCESS)
+        {
+            rc = nz_graph_add_block(graph, "1", "constant(real, 20)", NULL); if(rc != NZ_SUCCESS) goto err;
+            rc = nz_graph_add_block(graph, "2", "debug(real)", &block_handle); if(rc != NZ_SUCCESS) goto err;
+            rc = debug_pull(block_handle); if(rc != NZ_SUCCESS) goto err;
+            rc = nz_graph_connect(graph, "1", "out", "2", "in"); if(rc != NZ_SUCCESS) goto err;
+            rc = debug_pull(block_handle); if(rc != NZ_SUCCESS) goto err;
+            rc = nz_graph_disconnect(graph, "1", "out", "2", "in"); if(rc != NZ_SUCCESS) goto err;
+            rc = debug_pull(block_handle); if(rc != NZ_SUCCESS) goto err;
+            err:
+            nz_graph_destroy(graph);
+        }
+        nz_context_destroy(context);
+    }
+    return rc;
+}
+
+int main()
+{
+    nz_rc rc = run();
+    if(rc != NZ_SUCCESS)
+    {
+        fprintf(stderr, "Noise error %d: %s\n", rc, nz_error_rc_str(rc));
+        fprintf(stderr, "File: %s line %d\n", nz_error_file, nz_error_line);
+        if(nz_error_string) {
+            fprintf(stderr, "%s\n", nz_error_string);
+            free(nz_error_string);
+        }
+        return 1;
+    }
+    return 0;
+}
+
