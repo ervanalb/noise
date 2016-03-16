1d8698fd99383ab4d8e5e2aa33baef12d50c5fca
Zach Banks <zjbanks@gmail.com>
2016-03-15 21:49:03 -0400

    Removing junk files

--- debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
-
-#include "noise.h"
-
-void nz_debug_print_graph(struct nz_node * node);
-void nz_debug_print_dot(struct nz_node * node, const char * filename);
-
-#endif
